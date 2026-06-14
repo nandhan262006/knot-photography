34d8b603da1d1427d7ce6103e783d5d0c7c711c7
nandhan262006 <nandhan262006@gmail.com>
2026-06-14 05:56:06 +0530

    Remove all Sanity CMS, fix gallery with static images

--- sanity/schemas/index.js
+++
@@ -1,6 +0,0 @@
-import about from './about'
-import bespokeService from './services'
-import gallery from './gallery'
-import kidsStudio from './studio3d'
-
-export const schemaTypes = [about, bespokeService, gallery, kidsStudio]
